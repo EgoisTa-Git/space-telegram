17ca3c76050da126417cc7b70f158e0d3c96bff7
EgoisTa <97385729+EgoisTa-Git@users.noreply.github.com>
2022-05-04 23:28:21 +0300

    Change type of parsing names

--- save_files.py
+++ save_files.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 import requests
-from urllib import parse
+from urllib.parse import urlparse
 
 DIR_NAME = 'images'
 
@@ -10,7 +10,7 @@ DIR_NAME = 'images'
 def save_images(images, image_dir=DIR_NAME):
     Path(f'{DIR_NAME}').mkdir(parents=True, exist_ok=True)
     for image_url in images:
-        url_parse = parse.urlparse(image_url)
+        url_parse = urlparse(image_url)
         image_name = os.path.basename(url_parse.path)
         with open(f'{image_dir}/{image_name}', 'wb') as file:
             file.write(requests.get(image_url).content)
