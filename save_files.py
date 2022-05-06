be1d20afbe8c81e3023d7d5a342e3a2d68a333f1
EgoisTa <97385729+EgoisTa-Git@users.noreply.github.com>
2022-05-06 22:47:03 +0300

    Check request status before saving

--- save_files.py
+++ save_files.py
@@ -10,7 +10,9 @@ DIR_NAME = 'images'
 def save_images(images, image_dir=DIR_NAME):
     Path(f'{DIR_NAME}').mkdir(parents=True, exist_ok=True)
     for image_url in images:
+        response = requests.get(image_url)
+        response.raise_for_status()
         url_parse = urlparse(image_url)
         image_name = os.path.basename(url_parse.path)
         with open(f'{image_dir}/{image_name}', 'wb') as file:
-            file.write(requests.get(image_url).content)
+            file.write(response.content)
