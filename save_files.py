37e3c68210538545cc4eb8b78d4e1e7ca23011b8
EgoisTa <97385729+EgoisTa-Git@users.noreply.github.com>
2022-05-08 14:34:27 +0300

    Revert "Change method of getting image name"
    This reverts commit a29a703629a7eb1ce3058b587fe8eef296a6196f.

--- save_files.py
+++ save_files.py
@@ -13,6 +13,6 @@ def save_images(images, image_dir=DIR_NAME):
         response = requests.get(image_url)
         response.raise_for_status()
         url_parse = urlparse(image_url)
-        image_name = os.path.split(url_parse.path)[1]
+        image_name = os.path.basename(url_parse.path)
         with open(f'{image_dir}/{image_name}', 'wb') as file:
             file.write(response.content)
